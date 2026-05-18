340035b8453a1bc871a04c31792aeb7f5cc18506
salman-collab <76528373+salman-collab@users.noreply.github.com>
2026-05-18 14:07:11 +0530

    Update test_decisionTree.py

--- backend/app/test_decisionTree.py
+++ backend/app/test_decisionTree.py
@@ -1,1 +1,8 @@
+from core.preprocessing import load_data, clean_data, encode_data, normalize, train_test_split
+from algorithms.linear_regression import LinearRegression
 
+df = load_data("data/churn.csv")
+df = clean_data(df)
+df = encode_data(df)
+df = normalize(df)
+print(df.head())
