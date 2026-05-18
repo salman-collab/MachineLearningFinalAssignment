85bca0357949375cf8992a4f7d30858ccffd9994
salman-collab <76528373+salman-collab@users.noreply.github.com>
2026-05-18 14:11:06 +0530

    Update test_decisionTree.py

--- backend/app/test_decisionTree.py
+++ backend/app/test_decisionTree.py
@@ -6,3 +6,20 @@ df = clean_data(df)
 df = encode_data(df)
 df = normalize(df)
 print(df.head())
+# Target column
+X = df.drop("Churn", axis=1).values.tolist()
+
+# Convert target into list
+y = df["Churn"].values.tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+model = DecisionTree(max_depth=5)
+model.fit(X_train, y_train)
+
+preds = model.predict(X_test)
+
+print("Sample Predictions:", preds[:5])
+
+print("\nDecision Tree:")
+print(model.tree)
